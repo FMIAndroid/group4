25671ce1ee50330777ea9eba200f907c6d79b938
Mladen Raev <mladensr@gmail.com>
2014-12-03 16:23:02 +0200

    add circle view

---
+++ ContactApp/src/bg/unisofia/fmi/contactapp/CircleView.java
@@ -0,0 +1,40 @@
+package bg.unisofia.fmi.contactapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Path.Direction;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CircleView extends ImageView {
+	
+	private Path path;
+	
+	private RectF rect;
+	
+	public CircleView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		path = new Path();
+		rect = new RectF();
+	}
+	
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		rect.left = 0;
+		rect.bottom = 0;
+		rect.right = w;
+		rect.bottom = h;
+		path.reset();
+		path.addOval(rect, Direction.CW);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.clipPath(path);
+		super.onDraw(canvas);
+	}
+
+}
